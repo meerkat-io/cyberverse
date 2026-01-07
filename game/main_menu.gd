e8471e109b5e2612a0a19a23b854235c82e23ec5
Chenzhuo Sang <Chenzhuo.sang@gmail.com>
2026-01-07 14:42:29 +0100

    update

--- game/main_menu.gd
+++ game/main_menu.gd
@@ -26,3 +26,11 @@ func _on_option_3_pressed() -> void:
 
 func _on_exit_2_pressed() -> void:
 	get_tree().quit() # Replace with function body.
+
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_inventory_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Ui/Inventory/Code_inevntory.tscn")
