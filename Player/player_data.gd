a7d0d6c928740995bc4b204852058f394e73ee6f
Chenzhuo Sang <Chenzhuo.sang@gmail.com>
2026-02-16 17:59:57 +0100

    update

---
+++ Player/player_data.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var diamonds: int = 0
--- game/shop/shop.gd
+++ game/shop/shop.gd
@@ -1,9 +1,11 @@
 extends Control
+@onready var rightmenu: PanelContainer = $menu3/rightmenu
+@onready var leftmenu: PanelContainer = $Node/leftmenu
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,5 +13,7 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_home_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Ui/main_menu.tscn")
+func _on_exit_pressed() -> void:
+	#rightmenu.visible = true
+	#leftmenu.visible = true
+	queue_free()
