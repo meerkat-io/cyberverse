2dc8a426c6395ac2b95673602f6cc7d29a487dfc
Sang <sang@miro.com>
2026-02-17 08:18:01 +0100

    update

--- code_blocks/scripts/drag_drop_area.gd
+++ code_blocks/scripts/drag_drop_area.gd
@@ -8,6 +8,8 @@ var _drag_offset: Vector2
 var _root_block: Node2D
 var _drag_start_position: Vector2 = Vector2.INF
 var _snap_points: Array[Control] = []
+var _connected_roots: Array[Node2D] = []
+var _connected_offsets: Array[Vector2] = []
 
 func _ready() -> void:
 	_root_block = _get_root_block()
@@ -34,9 +36,7 @@ func _process(_delta: float) -> void:
 
 	if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		if _is_dragging or _drag_start_position != Vector2.INF:
-			_is_dragging = false
-			_drag_start_position = Vector2.INF
-			_snap_points.clear()
+			_on_drag_ended()
 		return
 
 	var current_mouse_pos = get_global_mouse_position()
@@ -46,7 +46,7 @@ func _process(_delta: float) -> void:
 			_is_dragging = true
 			_drag_offset = _root_block.global_position - current_mouse_pos
 			get_viewport().set_input_as_handled()
-			_update_snap_points()
+			_on_drag_started()
 
 	if _is_dragging:
 		var target_pos = current_mouse_pos + _drag_offset
@@ -56,6 +56,58 @@ func _process(_delta: float) -> void:
 			_root_block.global_position = closest_snap_point.global_position
 		else:
 			_root_block.global_position = target_pos
+			
+		for i in range(_connected_roots.size()):
+			if is_instance_valid(_connected_roots[i]):
+				_connected_roots[i].global_position = _root_block.global_position + _connected_offsets[i]
+
+func _on_drag_started() -> void:
+	_update_snap_points()
+	
+	var my_block = _get_block()
+	if my_block:
+		var head = my_block.get_head_block()
+		if head:
+			head.set_tail_block(null)
+			my_block.set_head_block(null)
+			
+		_connected_roots.clear()
+		_connected_offsets.clear()
+		
+		var curr = my_block.get_tail_block()
+		while curr:
+			var root = _get_root_of_block(curr)
+			if root:
+				_connected_roots.append(root)
+				_connected_offsets.append(root.global_position - _root_block.global_position)
+			curr = curr.get_tail_block()
+
+func _on_drag_ended() -> void:
+	if _is_dragging:
+		var closest_snap_point = _get_closest_snap_point(_root_block.global_position)
+		if closest_snap_point:
+			var target_block = _get_block_snap_point(closest_snap_point)
+			var my_block = _get_block()
+			
+			if target_block and my_block and target_block != my_block:
+				var old_tail = target_block.get_tail_block()
+				
+				target_block.set_tail_block(my_block)
+				my_block.set_head_block(target_block)
+				
+				if old_tail:
+					var my_last = my_block
+					while my_last.get_tail_block():
+						my_last = my_last.get_tail_block()
+					
+					my_last.set_tail_block(old_tail)
+					old_tail.set_head_block(my_last)
+
+	_is_dragging = false
+	_drag_start_position = Vector2.INF
+	_snap_points.clear()
+	_connected_roots.clear()
+	_connected_offsets.clear()
 
 func _update_snap_points() -> void:
 	_snap_points.clear()
@@ -86,3 +138,12 @@ func _get_block() -> BaseBlock:
 
 func _get_block_content() -> BlockContent:
 	return _get_block().get_content()
+
+func _get_block_snap_point(p_snap_point: Control) -> BaseBlock:
+	var parent = p_snap_point.get_parent()
+	while parent and not parent is BaseBlock:
+		parent = parent.get_parent()
+	return parent as BaseBlock
+
+func _get_root_of_block(p_block: BaseBlock) -> Node2D:
+	return p_block.get_parent() as Node2D
