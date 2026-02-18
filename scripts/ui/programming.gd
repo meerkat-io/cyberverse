66ddb74a9a3f95f011da2edee94b40a6a9cd7bcd
Sang <sang@miro.com>
2026-02-18 20:46:23 +0100

    update

---
+++ scripts/ui/programming.gd
@@ -0,0 +1,11 @@
+class_name Programming
+extends Control
+
+func _ready() -> void:
+	pass
+
+
+#	var test_data = {
+#		"Engine": {"Set Power": "res://code_blocks/scenes/blocks/engine/set_power.tscn"},
+#		"Control": {"On Start": "res://code_blocks/scenes/blocks/entry/on_start.tscn"}
+#	}
